1dad37db2419ba98bff618ad8c8e824ce4875eae
anchen.li <anchen.li@vedaleon.com>
2016-02-02 11:51:11 +1100

    init

---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var notifier = require('node-notifier');
+var server = require('gulp-server-livereload');
+var concat = require('gulp-concat');
+var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
+var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var envify = require('loose-envify/custom');
+
+
+var config = {
+    paths:{
+      css:[
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+            './sass/**/*.scss'
+            ],
+
+    }
+
+};
+
+
+var notify = function(error) {
+  var message = 'In: ';
+  var title = 'Error: ';
+
+  if(error.description) {
+    title += error.description;
+  } else if (error.message) {
+    title += error.message;
+  }
+
+  if(error.filename) {
+    var file = error.filename.split('/');
+    message += file[file.length-1];
+  }
+
+  if(error.lineNumber) {
+    message += '\nOn Line: ' + error.lineNumber;
+  }
+
+  notifier.notify({title: title, message: message});
+};
+
+var bundler = watchify(browserify({ entries: './src/app.jsx', extensions: ['.jsx'], debug: true })
+    .transform('babelify', { presets: ['es2015', 'react'] }))
+    .transform('envify',{'global': true, NODE_ENV: 'production'});
+;
+
+
+function bundle() {
+  return bundler
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('main.js'))
+    .pipe(buffer())//for minify
+    .pipe(uglify())//
+    .pipe(gulp.dest('./'))
+}
+bundler.on('update', bundle)
+
+gulp.task('build', function() {
+  bundle()
+});
+
+gulp.task('serve', function(done) {
+  gulp.src('')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        filter: function(filePath, cb) {
+          if(/main.js/.test(filePath)) {
+            cb(true)
+          } else if(/style.css/.test(filePath)){
+            cb(true)
+          }
+        }
+      },
+      open: true
+    }));
+});
+
+gulp.task('sass', function () {
+  sass(config.paths.css, { style: 'expanded' })
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['build', 'serve', 'sass', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
